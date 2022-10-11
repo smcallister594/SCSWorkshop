1dbd0a3b09b0765dad9ed5d940ffe2002f7fc3df
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-10-11 11:54:45 +0000

    should fix the bicep errors

--- .devcontainer/postCreateCommand.ps1
+++ .devcontainer/postCreateCommand.ps1
@@ -5,19 +5,6 @@ $modules =  'Az.Accounts', 'Az.Resources', 'Az.Storage', 'Az.KeyVault'
 foreach($module in $modules){
     Install-Module $module
 }
-$oldFile = '/workspaces/SCSWorkshop/CleanUp/oldresourcelist.txt'
-$resourcesFile = '/workspaces/SCSWorkshop/CleanUp/resourcelist.ps1'
-$message = '
-# previous resources {0}
-' -f (Get-Date).ToLongDateString()
-
-$message | Out-File -FilePath $oldFile -Append
-
-Get-Content $resourcesFile | Out-File -FilePath $oldFile -Append
-
-$message = '# resources created - {0}' -f (Get-Date).ToLongDateString()
-
-$message |Out-File -FilePath $resourcesFile
 
 # mark this as a safe directory for git to play nice
 git config --global --add safe.directory /workspaces/SCSWorkshop
